333b732ccdf380ab034f2694d46a86155f039dd3
SivaprasadTamatam <cpp.sivaprasad@gmail.com>
2026-01-17 14:51:28 +0530

    insertion sort program as part of ds & algs part

---
+++ data structures - algorithims/insertion_sort.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func insertionSort(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j = j - 1
+		}
+		arr[j+1] = key
+	}
+	return arr
+}
+
+func main() {
+	fmt.Println(insertionSort([]int{5, 2, 9, 1, 3, 6}))
+}
